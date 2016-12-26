43f2e42ed1304d67c3f4b25ea64148f4c32672e2
Alper Gündogdu <alpergundogdu@alpers-macbook-pro.local>
2016-12-26 18:59:01 +0300

    Initial Commit

---
+++ TimeEditLNUTests/TimeEditLNUTests.swift
@@ -0,0 +1,36 @@
+//
+//  TimeEditLNUTests.swift
+//  TimeEditLNUTests
+//
+//  Created by Alper Gündogdu on 12/26/16.
+//  Copyright © 2016 Alper Gündogdu. All rights reserved.
+//
+
+import XCTest
+@testable import TimeEditLNU
+
+class TimeEditLNUTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
